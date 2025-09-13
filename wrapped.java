4f6a19ee127da549df85bc23547bb9a4bbbbdb21
TreePlanter <lemurkanikitka@mail.ru>
2025-09-13 08:51:03 +0300

    add wrapped prototype

---
+++ wrapped.java
@@ -0,0 +1,12 @@
+public class wrapped {
+    public class Chunk {
+    public
+        int qualityIndex;
+        String streetName;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Goodbye world!");
+    }
+}

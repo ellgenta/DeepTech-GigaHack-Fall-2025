6a3f911f5f1052dac681b016c52ab1eb65958e25
TreePlanter <lemurkanikitka@mail.ru>
2025-09-13 09:01:18 +0300

    update wrapped

--- wrapped.java
+++ wrapped.java
@@ -1,10 +1,23 @@
 public class wrapped {
+    public class ChunkInfo {
+    public
+        double latitude;
+        double longtitude;
+        double length;
+        int placeId;
+        String streetName;
+    }
+
     public class Chunk {
     public
         int qualityIndex;
-        String streetName;
+        
 
     }
+
+    /*
+     * 
+     */
 
     public static void main(String[] args) {
         System.out.println("Goodbye world!");

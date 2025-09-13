4b3f9e197783fc304a00d5450ce61a2e276a2a84
ellgenta <lemurkanikitka@mail.ru>
2025-09-13 11:59:28 +0300

    Update wrapped.java

--- wrapped.java
+++ wrapped.java
@@ -1,31 +1,45 @@
 public class wrapped {
     public class ChunkInfo {
-        private double latitude;
-        private double longtitude;
-        private double length;
-        private int placeId;
-        private String streetName;
+        double latitude; //нам это не нужнл
+        double longtitude;
+        double length;
+        int placeId;
+        int balance; //баланс оценок на фото
+        String congestionLevel; //трафик (пробки: medium, low, high) 
+        String streetName;
         
-        private class Quality {
-            int qualityIndex;
-            int deviationCount; //monthly average
-            int castedBalance; //average upvote - downvote i.e. upvCount + dwnCount / Count
+        public class ChunkProperties {}
+            double deviation = 0.0; //среднее отклонение для участка дороги [0, 3]
+            double rating = 0.0; //"средняя звезда" из приложение [0, 1.2]
+            double photo = 0.0; //текущее количество фото [0.8]
+        };
+
+        public double getCongestionCoef() {
+            if(congestion == "low")
+                return 0.25;
+            if(congestion == "medium")
+                return 0.7;
+            return 1.0;
+        }
+
+        public double getPhotoMark() {
+            if(photo > 10)
+                return 0.8;
+            if(photo > 5)
+                return 0.4;
+            return 0.0;
+        }
+
+        public double getIndex() {
+            double index = 0.0; //5 - хуево, 0 - отлично
             
-            public double getIndex() {
+            double actualDeviation = deviation >= 3 ? 3 : deviation;
+            double actualRating = rating / 1.2;
+            double actualPhoto = getPhotoMark();
+            double congCoef = getCongestionCoef();
 
-                return 0.0;
-            }
+            index = congCoef * actualDeviation + actualRating + actualPhoto;
 
-        } 
-
-        //how to get this shit?
-    }
-
-    /*
-     * 
-     */
-
-    public static void main(String[] args) {
-        System.out.println("Goodbye world!");
-    }
+            return;
+        }
 }
